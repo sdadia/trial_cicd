baa6b9a08adeb49adc18b282b11f8a8db658c5b7
sdadia <dadiasahil94@yahoo.in>
2019-07-24 20:44:18 +0100

    second commit for CICD

---
+++ mango2.py
@@ -0,0 +1,15 @@
+def toga(a,b):
+    '''
+    This does nothing
+
+    Parameters
+    ----------
+    
+    a : str
+        first var
+    b : str
+        second var
+    '''
+    return a+b
+
+
---
+++ test_mango2.py
@@ -0,0 +1,11 @@
+import unittest
+from mango2 import toga
+
+class TestMango2(unittest.TestCase):
+	def test_toga(self):
+		self.assertEqual(3, toga(1,2))
+		self.assertEqual(0, toga(0,0))
+		self.assertEqual(-1, toga(0,-1))
+
+if __name__ == '__main__':
+    unittest.main()
